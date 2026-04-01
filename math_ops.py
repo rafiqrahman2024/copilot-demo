0ecd0b3543041ab7b96c93964b02d85b6556967e
Rafiq <rafiqrahman26@gmail.com>
2026-04-01 16:56:30 +0530

    Add math_ops.py with add and subtract functions

---
+++ math_ops.py
@@ -0,0 +1,16 @@
+ ```python
+"""This module contains basic mathematical operations."""
+
+def add(a, b):
+    """Adds two numbers together."""
+    return a + b
+
+
+def subtract(a, b):
+    """Subtracts one number from another."""
+    return a - b
+``` 
+
+In the above Python code, we have created a simple module named `math_ops.py`. It contains two functions: add and subtract. Both of these functions take two arguments (a and b) and return their sum and difference respectively. The docstrings in this case are used to provide documentation for both functions.
+
+
