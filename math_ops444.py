7e96ff960fe16150ea4001a67382339b59b34758
Rafiq <rafiqrahman26@gmail.com>
2026-04-01 18:07:11 +0530

    Add math_ops444.py with add and subtract functions

---
+++ math_ops444.py
@@ -0,0 +1,15 @@
+ 
+```python
+"""Addition operation."""
+def add(a, b):
+    return a + b
+
+
+"""Subtraction operation."""
+def subtract(a, b):
+    return a - b
+```
+
+Note that the function names and parameters are self-explanatory because I have included docstrings. You can call these functions using add() or subtract(), providing two numbers as arguments.
+
+
